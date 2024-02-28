2bf588f87913445874997251d14cd58ae2ab5d06
Viola Bergere <viola.bergere@gmail.com>
2024-02-28 20:42:52 +0000

    Add links in urls.py and base.html

--- wine_cellar/urls.py
+++ wine_cellar/urls.py
@@ -3,10 +3,14 @@ from .views import home
 from .views import about
 from .views import contact
 from .views import gallery
+from .views import password_reset, signup, login
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('gallery/', gallery, name='gallery'),
+    path('password_reset/', password_reset, name='password_reset'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
 ]
