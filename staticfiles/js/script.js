359e2f3d5866d57d2710c3ff203d69cb3102e64b
Viola Bergere <viola.bergere@gmail.com>
2024-02-28 15:06:49 +0000

    Move favicon to static/images

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,14 +1,9 @@
-const loader = new Loader({
-  apiKey: "YOUR_API_KEY",
-  version: "weekly",
-  ...additionalOptions,
-});
-
-loader.load().then(async () => {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 45.061724, lng: -0.887769 },
-    zoom: 8,
+function initMap() {
+  const loader = new google.maps.Marker({
+    position: { lat: 45.061724, lng: -0.887769 },
+    map: new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 45.061724, lng: -0.887769 },
+      zoom: 14,
+    }),
   });
-});
+}
