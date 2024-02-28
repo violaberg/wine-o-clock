471038034e3f6bc259d96f3d5226dcd9968f83a3
Viola Bergere <viola.bergere@gmail.com>
2024-02-28 13:15:31 +0000

    Style gallery,add admin function for delete/edit gallery

--- wine_cellar/admin.py
+++ wine_cellar/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from .models import GalleryImage
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Register your models here.
-@admin.register(GalleryImage)
 class GalleryImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description']
+    list_display = ['id', 'description', 'image_thumbnail']
     search_fields = ['description']
     list_per_page = 20
+    actions = ['edit_selected_image']
+
+    def image_thumbnail(self, obj):
+        return '<img src="{}" width="50" height="50" />'.format(obj.image.url)
+
+    image_thumbnail.allow_tags = True
+    image_thumbnail.short_description = 'Image'
+
+    def edit_selected_image(self, request, queryset):
+        if queryset.count() == 1:
+            selected_id = queryset[0].id
+            edit_url = reverse ('admin:wine_cellar_galleryimage_change', args=[selected_id])
+            return HttpResponseRedirect(edit_url)
+        else:
+            self.message_user(request, 'Please select only one image to edit.', level='warning')
+    
+    edit_selected_image.short_description = 'Edit selected image'
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        return super().change_view(request, object_id, form_url, extra_context)
+
+admin.site.register(GalleryImage, GalleryImageAdmin)
