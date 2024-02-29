40c36b46ab51c91cba107a5a34d632c5e95d11eb
Viola Bergere <viola.bergere@gmail.com>
2024-02-29 11:12:26 +0000

    Delete unneeded images

--- my_project/settings.py
+++ my_project/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
