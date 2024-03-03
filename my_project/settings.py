93fb18c3eaf707fe6a45ef8c0567ef1dc0a9f871
Viola Bergere <viola.bergere@gmail.com>
2024-03-03 18:33:49 +0000

    Add media files to settings.py  and urls.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -146,6 +146,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wine_cellar/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
