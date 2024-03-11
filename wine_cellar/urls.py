58ae508508b9a8258e1007f45116006199dd8f15
Viola Bergere <viola.bergere@gmail.com>
2024-03-11 16:15:24 +0000

    Update urls.py

--- wine_cellar/urls.py
+++ wine_cellar/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import home
 from .views import about
-from .views import contact, contact_form_success
+from .views import contact
 from .views import gallery
-from .views import reviews
+from .views import display_review
 
 
 urlpatterns = [
@@ -11,6 +11,5 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('gallery/', gallery, name='gallery'),
-    path('reviews/', reviews, name='reviews'),
-    path('contact_form_success/', contact_form_success, name='contact_form_success'),
+    path('reviews/', display_review, name='display_review'),
 ]
