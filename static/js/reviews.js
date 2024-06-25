a2b6bb1ff08fc69682254ae2bfa8c02860eccd55
Viola Bergere <viola.bergere@gmail.com>
2024-06-25 11:13:30 +0000

    Add scroll to form function for review edit

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,22 +1,31 @@
-const editButtons = document.getElementsByClassName("review-btn-edit");
+const editReviewButtons = document.getElementsByClassName("review-btn-edit");
 const reviewText = document.getElementById("id_body");
 const reviewForm = document.getElementById("reviewForm");
-const submitButton = document.getElementById("submitButton");
+const submitReviewButton = document.getElementById("submitReviewButton");
 
 const deleteReviewModal = new bootstrap.Modal(document.getElementById("deleteReviewModal"));
 const deleteReviewButtons = document.getElementsByClassName("review-btn-delete");
 const deleteReviewConfirm = document.getElementById("deleteReviewConfirm");
 
+
+// Function to scroll to the review form
+function scrollToReviewForm() {
+  reviewForm.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+
 /**
-* Initializes edit functionality for the provided edit buttons.
-* 
-* For each button in the `editButtons` collection:
-* - Retrieves the associated review's ID upon click.
-* - Fetches the content of the corresponding review.
-* - Populates the `reviewText` input/textarea with the review's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_review/{reviewId}` endpoint.
-*/
+ * Initializes edit functionality for the provided edit buttons.
+ * 
+ * For each button in the `editReviewButtons` collection:
+ * - Retrieves the associated review's ID upon click.
+ * - Fetches the content of the corresponding review.
+ * - Populates the `reviewText` input/textarea with the review's content for editing.
+ * - Updates the submit button's text to "Update".
+ * - Sets the form's action attribute to the `edit_review/{reviewId}` endpoint.
+ */
 for (let button of editReviewButtons) {
   button.addEventListener("click", (e) => {
     let reviewId = e.target.getAttribute("data-review_id");
@@ -24,19 +33,21 @@ for (let button of editReviewButtons) {
     reviewText.value = reviewBody;
     submitReviewButton.innerText = "Update";
     reviewForm.setAttribute("action", `edit_review/${reviewId}`);
+    scrollToReviewForm();
   });
 }
 
+
 /**
-* Initializes deletion functionality for the provided delete buttons.
-* 
-* For each button in the `deleteButtons` collection:
-* - Retrieves the associated review's ID upon click.
-* - Updates the `deleteConfirm` link's href to point to the 
-* deletion endpoint for the specific review.
-* - Displays a confirmation modal (`deleteModal`) to prompt 
-* the user for confirmation before deletion.
-*/
+ * Initializes deletion functionality for the provided delete buttons.
+ * 
+ * For each button in the `deleteButtons` collection:
+ * - Retrieves the associated review's ID upon click.
+ * - Updates the `deleteConfirm` link's href to point to the 
+ * deletion endpoint for the specific review.
+ * - Displays a confirmation modal (`deleteModal`) to prompt 
+ * the user for confirmation before deletion.
+ */
 for (let button of deleteReviewButtons) {
   button.addEventListener("click", (e) => {
     let reviewId = e.target.getAttribute("data-review_id");
